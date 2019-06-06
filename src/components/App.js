859acfcf2ec92e0818c72847480dd5c33d2965d4
Payton Lower <paytown@me.com>
2019-06-06 11:15:38 -0700

    services in progress

--- src/components/App.js
+++ src/components/App.js
@@ -2,16 +2,6 @@ import React from 'react';
 import GraveList from './graves/GraveList';
 
 export default function App() {
-  // const graveyardList = [
-  //   {
-  //     name: 'Motel 666',
-  //     id: '1abc'
-  //   }, {
-  //     name: 'Hotel California',
-  //     id: 'shoop3'
-  //   }
-  // ];
-
   const graveList = [
     {
       occupied: false,
--- src/services/fetchApi.js
+++ src/services/fetchApi.js
@@ -1,17 +1,35 @@
-// FIX FETCH ID AND LOCATION OBJECT
+// LINE 4 13 20 31
 
 export const getGraveyardList = () => {
-  return fetch('URL!!!!')
+  // return fetch('URL!!!!')
     .then(res => ([res.ok, res.json()]))
     .then(([ok, json]) => {
-      if(!ok) throw 'Unable to fetch';
+      if(!ok) throw 'Unable to fetch graveyard list';
       return json;
     })
     .then(json => json.map(graveyard => ({
       id: graveyard._id,
       name: graveyard.name,
-      location: { LOOK_AT_OBJECT: 'OK' },
-      total: graveyard.total,
-      occupied: graveyard.occupied
+      // location: { LOOK_AT_OBJECT: 'OK' },
+      totalGraves: graveyard.totalGraves,
+      occupiedGraves: graveyard.occupiedGraves
+    })));
+};
+
+export const getGraveList = () => {
+  // return fetch('URL!!!!')
+    .then(res => ([res.ok, res.json()]))
+    .then(([ok, json]) => {
+      if(!ok) throw 'Unable to fetch grave list';
+      return json;
+    })
+    .then(json => json.map(grave => ({
+      id: grave._id,
+      occupied: grave.occupied,
+      occupant: {
+        name: grave.occupant.name
+        // WHAT ELSE IS IN HERE??
+      },
+      graveyard: grave.graveyard
     })));
 };
